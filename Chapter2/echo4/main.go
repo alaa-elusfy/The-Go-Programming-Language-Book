85b66d981f34ff962f5653039261fd536df6df7b
Alaa Yousfi <alaaelusfy@gmail.com>
2026-01-02 08:19:41 +0100

    Chapter 2 examples from section 1 to 2.5

---
+++ Chapter2/echo4/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var n = flag.Bool("n", false, "Omit trailing newline")
+var sep = flag.String("s", " ", "Separator")
+
+func main() {
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+
+	if !*n {
+		fmt.Println()
+	}
+}
+
+// go run main.go -h (-help) >> Help(By default)
